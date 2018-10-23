3f85979cd51ca881c82c3c024d4470ed36d0c65e
Dave Randall <dave@goldsquare.co.uk>
2018-10-23 20:29:19 +0100

    Output is now aligned by team name

--- hatstats/__init__.py
+++ hatstats/__init__.py
@@ -2,15 +2,16 @@ from hatstats import load_csv
 from hatstats import team
 
 team_list = load_csv.load_teams()
+team_chars = len(max(team_list, key=lambda team: len(team.name)).name)
 
 print('\n*Average player salary (total players)*')
 sorted_list = sorted(team_list, key=lambda team: team.avg_salary, reverse=True)
 for team in sorted_list:
-    print('```{} - £{:,.2f} ({})```'.format(team.name, team.avg_salary, team.player_count))
+    print('```{0:{1}} - £{2:,.2f} ({3})```'.format(team.name, team_chars, team.avg_salary, team.player_count))
 
 print('\n*Average player age*')
 sorted_list = sorted(team_list, key=lambda team: (team.avg_age.years, team.avg_age.days), reverse=True)
 for team in sorted_list:
-    print('```{} - {} ({})```'.format(team.name, team.avg_age.years, team.avg_age.days))
+    print('```{0:{1}} - {2} ({3})```'.format(team.name, team_chars, team.avg_age.years, team.avg_age.days))
 
 print('')
